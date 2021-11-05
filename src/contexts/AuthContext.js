9cead12de7230482da00ab2d5eaeaed3ca2df2dc
bensabri <ssabril@ymail.com>
2021-11-05 07:47:10 +0100

    changing the link in the sent email link

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -30,7 +30,7 @@ export const AuthProvider = ({ children }) => {
 	const actionCodeSettings = {
 		// URL you want to redirect back to. The domain (www.example.com) for this
 		// URL must be whitelisted in the Firebase Console.
-		url: 'http://localhost:3000/confirm',
+		url: 'https://unsa-fcs.fr/confirm',
 		// This must be true.
 		handleCodeInApp: true,
 	};
